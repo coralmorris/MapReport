8c109ca23fb82dd2d47b42fecb92e14aa7b941e9
coralmorris <coral.morris33@gmail.com>
2015-06-01 09:21:07 -0400

    map cases report

---
+++ MapReport/js/main.js
@@ -0,0 +1,136 @@
+﻿
+var myApp = angular.module('myApp', ['ngMap', 'passdown']);
+myApp.controller("appController", function ($scope, myService) {
+    $scope.currentTime = new Date().getTime();
+
+    $scope.arList = [];
+    $scope.kmlList = [];
+
+    $scope.columnDefinition = [
+        { title: 'REPORT NUMBER', attributeName: 'caseNumber' }
+    ];
+
+    $scope.reportSelected = function (selectedReport) {
+        console.log('reportSelected()');
+        console.log(selectedReport);
+    }
+
+
+    var baseURL = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/MapCasesReport/kml/";
+
+    console.log(window.location.protocol);
+    console.log(window.location.hostname);
+    console.log(window.location.port);
+    //    
+    //        function displayAll() {
+    //        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //            center: new google.maps.LatLng(-0, 0),
+    //            zoom: 2,
+    //            mapTypeId: google.maps.MapTypeId.TERRAIN
+    //        });
+    //        for (i = 0; i < $scope.caseList.length; i++) {
+    //            var kmlURL = baseURL + $scope.caseList[i].case_id + ".kml";
+    //            var myParser = new geoXML3.parser({map: map});
+    //            myParser.parse(kmlURL);
+    //        }
+    //    }
+    function displayAll() {
+        var arAssociativeArray = {};
+        for (i = 0; i < $scope.arList.length; i++) {
+            //console.log($scope.arList[i]);
+
+            var arId = $scope.arList[i].caseId;
+
+            $scope.arList[i].kmlUrl = baseURL + arId + ".kml";
+            arAssociativeArray[arId] = $scope.arList[i];
+
+            $scope.mapListItems = arAssociativeArray;
+            console.log($scope.mapListItems);
+        }
+    }
+
+
+
+
+    /* Gets list of cases from service */
+    getCases = function () {
+        myService.getCases().then(function (data) {
+            $scope.arList = data;
+            displayAll();
+        }, function (status) {
+            console.log(status)
+        });
+    }
+
+    // Populate cases when we enter controller
+
+
+    $scope.showInContextWindow = function (event) {
+        $scope.name = event.featureData.name;
+        $scope.description = event.featureData.description;
+    };
+
+    // Create map and display all of the selected KLMs on it using geoXML3
+    var displayKMLs = function () {
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+            center: new google.maps.LatLng(-0, 0),
+            zoom: 2,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        });
+        var myParser = new geoXML3.parser({ map: map });
+        for (var i = 0; i < $scope.kmlList.length; i++) {
+            myParser.parse($scope.kmlList[i]);
+        }
+    };
+
+    $scope.viewKML = function (id) {
+        var kmlURL = baseURL + id + ".kml";
+        if (document.getElementById(id).style.fontWeight !== "bolder") {
+            var addToList = true;
+            for (var i = 0; i < $scope.kmlList.length; i++) {
+                if ($scope.kmlList[i] === kmlURL) {
+                    addToList = false;
+                }
+            }
+            if (addToList) {
+                $scope.kmlList.push(kmlURL);
+            }
+            displayKMLs();
+            console.log("adding KML-layer for " + id)
+            document.getElementById(id).style.fontWeight = "bolder";
+        }
+        else {
+            console.log("removing KML-layer for " + id)
+            for (var i = 0; i < $scope.kmlList.length; i++) {
+                if (kmlURL === $scope.kmlList[i]) {
+                    $scope.kmlList.splice(i, 1);
+                    document.getElementById(id).style.fontWeight = "";
+                }
+            }
+            displayAll();
+        }
+        console.log("Number of KML-layers being displayed = " + $scope.kmlList.length)
+        console.table($scope.kmlList);
+    };
+    //lets get the data that was sent over via the caseList widget
+    OWF.ready(function () {
+        console.log("Launch Data");
+        var arList = OWF.Launcher.getLaunchData();
+        console.log(arList);
+        if (arList && arList.length) {
+            $scope.arList = arList;
+            displayAll();
+        }
+        else {
+            getCases();
+        }
+    });
+})
+
+
+
+
+
+
+
+
---
+++ MapReport/js/services.js
@@ -0,0 +1,91 @@
+﻿myApp.factory('myService', function ($http, $q) {
+
+    var cases = [
+        { case_id: 66113, case_status_short_desc: "DEVELOPING" },
+        { case_id: 66114, case_status_short_desc: "ACTIVE" },
+        { case_id: 66115, case_status_short_desc: "PENDING" },
+        { case_id: 66116, case_status_short_desc: "TARGETED" },
+        { case_id: 66117, case_status_short_desc: "DEVELOPING" },
+        { case_id: 66118, case_status_short_desc: "ACTIVE" },
+        { case_id: 66119, case_status_short_desc: "PENDING" },
+        { case_id: 66112, case_status_short_desc: "TARGETED" }
+    ];
+
+
+    /*
+   var getCases = function()
+   {
+       return cases;
+   };
+   */
+
+    var getCases = function () {
+        var deferred = $q.defer();
+        deferred.resolve(cases);
+        return deferred.promise;
+        /* Use this when restful services is working
+        $http.get("some url").success(function (data) {
+            cases.length = 0;
+            for (var s in data) {
+                cases.push(data[s])
+            }
+            deferred.resolve(cases);
+        }).error(function (data, status) {
+
+            deferred.reject();
+        })
+        return deferred.promise;
+        */
+    }
+
+
+    // This will change to use promise when calling database
+    var findCase = function (id) {
+        var singleCase = null;
+        for (var i = 0; i < cases.length; i++) {
+            singleCase = cases[i];
+            if (singleCase.case_id === id) {
+                return singleCase;
+            }
+        }
+    };
+
+    /* This will change to use promise when calling database
+    var getCase = function(id)
+    {
+        var singleCase = null;
+        singleCase = findCase(id);
+        return singleCase;
+    }
+    */
+
+    var getCase = function (id) {
+        var deferred = $q.defer();
+        var singleCase = null;
+        singleCase = findCase(id);
+        deferred.resolve(singleCase);
+        return deferred.promise;
+        /* Use when restful services gets resolved
+        $http.get("url/"+id).success(function (data) {
+            singleCase = data;
+            
+            deferred.resolve(singleCase);
+        }).error(function (data, status) {
+            console.log(status);
+            deferred.reject();
+        });
+        return deferred.promise;
+        */
+    }
+
+
+    return {
+        getCases: getCases,
+        getCase: getCase
+    };
+
+});
+
+
+
+
